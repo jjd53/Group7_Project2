870ef5561ba21892d54e869d73f68dab16e02170
jjd53 <jjd53@pitt.edu>
2025-01-30 21:02:03 -0500

    Completed Geometry class/method

---
+++ Geometry.py
@@ -0,0 +1,20 @@
+import numpy as np
+class Geometry:
+    def __init__(self, name, xa, ya, xb, yb, xc, yc):
+        self.name: str = name
+        self.xa: float = xa
+        self.ya: float = ya
+        self.xb: float = xb
+        self.yb: float = yb
+        self.xc: float = xc
+        self.yc: float = yc
+
+
+    def Deq(self):
+
+        Dab = np.sqrt((self.xb - self.xa)**2 + (self.yb - self.ya)**2)
+        Dbc = np.sqrt((self.xc - self.xb) ** 2 + (self.yc - self.yb) ** 2)
+        Dca = np.sqrt((self.xa - self.xc) ** 2 + (self.ya - self.yc) ** 2)
+
+        self.Deq = np.cbrt(Dab * Dbc * Dca)
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Bundle import Bundle
 from Conductor import Conductor
+from Geometry import Geometry
 
 conductor1 = Conductor("Partridge", 0.642, 0.0217,
 0.385, 460)
@@ -13,3 +14,8 @@ print(bundle1.name, bundle1.num_conductors, bundle1.spacing, bundle1.conductor.n
 bundle1.calcradii()
 print(bundle1.DSC, bundle1.DSL)
 
+geometry1 = Geometry("Geometry 1", 0, 0, 18.5, 0, 37, 0)
+print(geometry1.name, geometry1.xa, geometry1.ya, geometry1.xb, geometry1.yb, geometry1.xc, geometry1.yc)
+
+geometry1.Deq()
+print(geometry1.Deq)
