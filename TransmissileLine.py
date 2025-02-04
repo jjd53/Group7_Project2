80b7f7d6a901723bbae0a0d139e98200cf8fa71b
Phankhanhdo <PHD26@pitt.edu>
2025-02-04 00:19:09 -0600

    Create TransmissileLine.py

---
+++ TransmissileLine.py
@@ -0,0 +1,20 @@
+# TransmissionLine.py
+class TransmissionLine:
+    def __init__(self, name: str, bus1: Bus, bus2: Bus, bundle, geometry, length: float):
+        self.name = name
+        self.bus1 = bus1
+        self.bus2 = bus2
+        self.bundle = bundle
+        self.geometry = geometry
+        self.length = length
+        
+        # Series impedance and admittance calculations
+        self.zseries = self.calc_series_impedance()
+        self.yshunt = self.calc_shunt_admittance()
+        self.yprim = None 
+
+    def calc_series_impedance(self):
+        return self.length * self.bundle.conductor.resistance
+
+    def calc_shunt_admittance(self):
+        return 0.01 * self.length
