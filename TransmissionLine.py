07b6f289754c25a653c3745e64886a496755a544
Phankhanhdo <PHD26@pitt.edu>
2025-02-05 00:43:06 -0600

    Update TransmissionLine.py

--- TransmissionLine.py
+++ TransmissionLine.py
@@ -1,20 +1,28 @@
 # TransmissionLine.py
+from Bus import Bus
 class TransmissionLine:
-    def __init__(self, name: str, bus1: str, bus2: str, bundle, geometry, length: float):
+    def __init__(self, name: str, bus1: Bus, bus2: Bus, bundle, geometry, length: float):
         self.name = name
         self.bus1 = bus1
         self.bus2 = bus2
         self.bundle = bundle
         self.geometry = geometry
         self.length = length
-        
-        # Series impedance and admittance calculations
-        self.zseries = self.calc_series_impedance()
-        self.yshunt = self.calc_shunt_admittance()
-        self.yprim = None 
 
-    def calc_series_impedance(self):
-        return self.length * self.bundle.conductor.resistance
+        # Line parameter calculations
+        self.r_series, self.x_series, self.b_shunt = self.calc_line_parameters()
+        self.zseries = self.r_series + 1j * self.x_series
+        self.yshunt = 1j * self.b_shunt
+        self.yprim = None  # Placeholder for milestone 3
 
-    def calc_shunt_admittance(self):
-        return 0.01 * self.length
+    def calc_line_parameters(self):
+        # Assuming the formulas are given in the reference materials
+        resistance_per_km = self.bundle.conductor.resistance
+        reactance_per_km = 0.1  # Placeholder; can be derived from geometry and inductance
+        susceptance_per_km = 0.01  # Placeholder; can be derived from geometry and capacitance
+
+        r_series = resistance_per_km * self.length
+        x_series = reactance_per_km * self.length
+        b_shunt = susceptance_per_km * self.length
+
+        return r_series, x_series, b_shunt
