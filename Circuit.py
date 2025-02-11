7937f455fadda9144410a73fc64b07652377d01e
jjd53 <jjd53@pitt.edu>
2025-02-11 08:36:52 -0500

    cleaned up code - no real changes

--- Circuit.py
+++ Circuit.py
@@ -25,13 +25,13 @@ class Circuit:
         self.conductors[name] = Conductor(name, diam, GMR, resistance, ampacity)
 
     def add_transformer(self, name, bus1, bus2, power_rating, impedance_percent, x_over_r_ratio):
-        self.transformers[name] = Transformer(name, bus1, bus2, power_rating, impedance_percent, x_over_r_ratio)
+        self.transformers[name] = Transformer(name, self.buses[bus1], self.buses[bus2], power_rating, impedance_percent, x_over_r_ratio)
 
-    def add_tline(self, name: str, bus1: Bus, bus2: Bus, bundle, geometry, length: float, frequency=60):
-        self.tlines[name] = TransmissionLine(name, bus1, bus2, bundle, geometry, length, frequency)
+    def add_tline(self, name: str, bus1: str, bus2: str, bundle, geometry, length: float, frequency=60):
+        self.tlines[name] = TransmissionLine(name, bus1, bus2, self.bundles[bundle], self.geometries[geometry], length, frequency)
 
     def add_bundle(self, name, num_conductors, spacing, conductor):
-        self.bundles[name]=Bundle(name, num_conductors, spacing, conductor)
+        self.bundles[name]=Bundle(name, num_conductors, spacing, self.conductors[conductor])
 
     def add_geometry(self, name, xa, ya, xb, yb, xc, yc):
         self.geometries[name]=Geometry(name, xa, ya, xb, yb, xc, yc)
--- Network.py
+++ Network.py
@@ -11,20 +11,20 @@ network.add_bus("bus5", 230)
 network.add_bus("bus6", 230)
 network.add_bus("bus7", 18)
 
-network.add_transformer("T1",network.buses["bus1"], network.buses["bus2"], 125, 8.5, 10)
-network.add_transformer("T2",network.buses["bus6"],network.buses["bus7"], 200, 10.5, 12)
+network.add_transformer("T1","bus1", "bus2", 125, 8.5, 10)
+network.add_transformer("T2","bus6","bus7", 200, 10.5, 12)
 
 network.add_conductor("Partridge", 0.642, 0.0217, 0.385, 460)
-network.add_bundle("Bundle 1", 2, 1.5, network.conductors["Partridge"])
+network.add_bundle("Bundle 1", 2, 1.5, "Partridge")
 network.add_geometry("Geometry 1", 0, 0, 18.5, 0, 37, 0)
 
 
-network.add_tline("L1","bus2", "bus4", network.bundles["Bundle 1"], network.geometries["Geometry 1"], 10, frequency=60)
-network.add_tline("L2","bus2", "bus3",network.bundles["Bundle 1"], network.geometries["Geometry 1"], 25, frequency=60)
-network.add_tline("L3","bus3", "bus5",network.bundles["Bundle 1"], network.geometries["Geometry 1"], 20, frequency=60)
-network.add_tline("L4","bus4", "bus6",network.bundles["Bundle 1"], network.geometries["Geometry 1"], 20, frequency=60)
-network.add_tline("L5","bus5", "bus6",network.bundles["Bundle 1"], network.geometries["Geometry 1"], 10, frequency=60)
-network.add_tline("L6","bus4", "bus5",network.bundles["Bundle 1"], network.geometries["Geometry 1"], 35, frequency=60)
+network.add_tline("L1","bus2", "bus4", "Bundle 1", "Geometry 1", 10, frequency=60)
+network.add_tline("L2","bus2", "bus3","Bundle 1", "Geometry 1", 25, frequency=60)
+network.add_tline("L3","bus3", "bus5","Bundle 1", "Geometry 1", 20, frequency=60)
+network.add_tline("L4","bus4", "bus6","Bundle 1", "Geometry 1", 20, frequency=60)
+network.add_tline("L5","bus5", "bus6","Bundle 1", "Geometry 1", 10, frequency=60)
+network.add_tline("L6","bus4", "bus5","Bundle 1", "Geometry 1", 35, frequency=60)
 
 
 print(network.__dict__)
