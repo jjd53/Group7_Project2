15c15bbe272f3ea772a4144aa945ab90a026fce8
jjd53 <jjd53@pitt.edu>
2025-02-12 21:06:10 -0500

    cleaned up code - no real changes

--- Geometry.py
+++ Geometry.py
@@ -13,8 +13,8 @@ class Geometry:
     def Deq(self):
 
         Dab = np.sqrt((self.xb - self.xa)**2 + (self.yb - self.ya)**2)
-        Dbc = np.sqrt((self.xc - self.xb) ** 2 + (self.yc - self.yb) ** 2)
-        Dca = np.sqrt((self.xa - self.xc) ** 2 + (self.ya - self.yc) ** 2)
+        Dbc = np.sqrt((self.xc - self.xb)**2 + (self.yc - self.yb)**2)
+        Dca = np.sqrt((self.xa - self.xc)**2 + (self.ya - self.yc)**2)
 
         self.Deq = np.cbrt(Dab * Dbc * Dca)
 
