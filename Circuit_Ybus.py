16ff4040c7b5d81338476ce3d7300b7cbb5ff2ed
Phankhanhdo <PHD26@pitt.edu>
2025-02-27 02:11:46 -0500

    Update Circuit_Ybus.py

--- Circuit_Ybus.py
+++ Circuit_Ybus.py
@@ -7,81 +7,90 @@ from Conductor import Conductor
 from Bundle import Bundle
 from Geometry import Geometry
 
+
 class Circuit_Ybus:
     def __init__(self, name: str):
+        """ Initializes the Circuit object with dictionaries to store components """
         self.name = name
-        self.buses = {}  
-        self.transformers = {}  
-        self.tlines = {} 
-        self.conductors = {} 
-        self.bundles = {}  
+        self.buses = {}
+        self.transformers = {}
+        self.conductors = {}
+        self.tlines = {}
+        self.bundles = {}
         self.geometries = {}
-        self.ybus = None  
+        self.ybus = None  # Will be computed later
 
-    # Add Bus
 
-    def add_bus(self, bus, base_kv, bus_type: str, vpu=1.0, delta=0.0):
-        self.buses[bus] = Bus(bus, base_kv, bus_type, vpu, delta)
+    def add_bus(self, name, base_kv):
+        """ Adds a Bus object to the circuit """
+        self.buses[name] = Bus(name, base_kv)
 
-    # Add Conductor
+
     def add_conductor(self, name, diam, GMR, resistance, ampacity):
+        """ Adds a Conductor object to the circuit """
         self.conductors[name] = Conductor(name, diam, GMR, resistance, ampacity)
 
-    # Add Transformer
-    def add_transformer(self, name, bus1, bus2, power_rating, impedance_percent, x_over_r_ratio):
-        if bus1 in self.buses and bus2 in self.buses:
-            self.transformers[name] = Transformer(name, self.buses[bus1], self.buses[bus2], power_rating, impedance_percent, x_over_r_ratio)
-        else:
-            raise ValueError(f"One or both buses '{bus1}', '{bus2}' not found in the circuit.")
 
-    # Add Bundle
     def add_bundle(self, name, num_conductors, spacing, conductor):
+        """ Adds a Bundle object using an existing conductor """
         if conductor in self.conductors:
             self.bundles[name] = Bundle(name, num_conductors, spacing, self.conductors[conductor])
         else:
-            raise ValueError(f"Conductor '{conductor}' not found. Please add it first.")
+            raise ValueError(f"Conductor '{conductor}' not found in the circuit.")
 
-    # Add Geometry
+
     def add_geometry(self, name, xa, ya, xb, yb, xc, yc):
+        """ Adds a Geometry object to the circuit """
         self.geometries[name] = Geometry(name, xa, ya, xb, yb, xc, yc)
 
-    # Add Transmission Line
-    def add_tline(self, name, bus1, bus2, bundle, geometry, length: float, frequency=60):
+
+    def add_transformer(self, name, bus1, bus2, power_rating, impedance_percent, x_over_r_ratio, V1, V2):
+        """ Adds a Transformer object between two buses """
+        if bus1 in self.buses and bus2 in self.buses:
+            self.transformers[name] = Transformer(name, self.buses[bus1], self.buses[bus2], power_rating, impedance_percent, x_over_r_ratio, V1, V2)
+        else:
+            raise ValueError(f"One or both buses '{bus1}', '{bus2}' not found in the circuit.")
+
+
+    def add_tline(self, name: str, bus1: str, bus2: str, bundle, geometry, length: float, frequency=60):
+        """ Adds a Transmission Line object between two buses """
         if bus1 in self.buses and bus2 in self.buses and bundle in self.bundles and geometry in self.geometries:
             self.tlines[name] = TransmissionLine(name, self.buses[bus1], self.buses[bus2], self.bundles[bundle], self.geometries[geometry], length, frequency)
         else:
-            raise ValueError(f"One or more components '{bus1}', '{bus2}', '{bundle}', '{geometry}' are missing from the circuit.")
+            raise ValueError(f"One or more components for Transmission Line '{name}' not found in the circuit.")
 
-    # Compute Ybus Admittance Matrix
+    # Compute Ybus Matrix
     def calc_ybus(self):
+        """ Constructs the Ybus matrix by summing Yprim matrices of transformers and transmission lines """
         num_buses = len(self.buses)
-        if num_buses == 0:
-            raise ValueError("No buses available to construct Ybus matrix.")
+        bus_names = list(self.buses.keys())
+
+        # Initialize an empty Ybus matrix
         Ybus_matrix = np.zeros((num_buses, num_buses), dtype=complex)
-        bus_list = list(self.buses.keys())
 
-        # Process Transformers
-        for tf in self.transformers.values():
-            tf_matrix = tf.YPrim_matrix  # Get Yprim matrix
-            bus1_idx = bus_list.index(tf.bus1.name)
-            bus2_idx = bus_list.index(tf.bus2.name)
+        # Process transformers
+        for transformer in self.transformers.values():
+            yprim = transformer.YPrim_matrix.values
+            i = bus_names.index(transformer.bus1.name)
+            j = bus_names.index(transformer.bus2.name)
 
-            Ybus_matrix[bus1_idx, bus1_idx] += tf_matrix.iloc[0, 0]
-            Ybus_matrix[bus1_idx, bus2_idx] += tf_matrix.iloc[0, 1]
-            Ybus_matrix[bus2_idx, bus1_idx] += tf_matrix.iloc[1, 0]
-            Ybus_matrix[bus2_idx, bus2_idx] += tf_matrix.iloc[1, 1]
+            # Add admittances to Ybus
+            Ybus_matrix[i, i] += yprim[0, 0]
+            Ybus_matrix[i, j] += yprim[0, 1]
+            Ybus_matrix[j, i] += yprim[1, 0]
+            Ybus_matrix[j, j] += yprim[1, 1]
 
-        # Process Transmission Lines
+        # Process transmission lines
         for line in self.tlines.values():
-            line_matrix = line.YPrim_matrix  # Get Yprim matrix
-            bus1_idx = bus_list.index(line.bus1.name)
-            bus2_idx = bus_list.index(line.bus2.name)
+            yprim = line.YPrim_matrix.values
+            i = bus_names.index(line.bus1.name)
+            j = bus_names.index(line.bus2.name)
 
-            Ybus_matrix[bus1_idx, bus1_idx] += line_matrix.iloc[0, 0]
-            Ybus_matrix[bus1_idx, bus2_idx] += line_matrix.iloc[0, 1]
-            Ybus_matrix[bus2_idx, bus1_idx] += line_matrix.iloc[1, 0]
-            Ybus_matrix[bus2_idx, bus2_idx] += line_matrix.iloc[1, 1]
+            # Add admittances to Ybus
+            Ybus_matrix[i, i] += yprim[0, 0]
+            Ybus_matrix[i, j] += yprim[0, 1]
+            Ybus_matrix[j, i] += yprim[1, 0]
+            Ybus_matrix[j, j] += yprim[1, 1]
 
-        # Store Ybus Matrix
-        self.ybus = pd.DataFrame(Ybus_matrix, index=bus_list, columns=bus_list)
-
+        # Store final Ybus matrix
+        self.ybus = pd.DataFrame(Ybus_matrix, index=bus_names, columns=bus_names)
