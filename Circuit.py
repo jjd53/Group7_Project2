495ea2cc1659c039d032deb2d3df15472c50c763
jjd53 <jjd53@pitt.edu>
2025-03-18 09:23:16 -0400

    push changes back to working state

--- Circuit.py
+++ Circuit.py
@@ -23,7 +23,6 @@ class Circuit:
         self.settings = Settings()
 
 
-
     def add_bus(self,bus,base_kv,bus_type:str, vpu = 1.0,delta = 0.0):
         self.buses[bus]=Bus(bus,base_kv, bus_type, vpu, delta)
 
